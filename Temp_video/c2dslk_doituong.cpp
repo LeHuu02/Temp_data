9c0e7e21fc69d10190d14c639b116abc4d7525d3
Le Minh Huu <cnleminhhuu.com>
2022-11-22 17:27:51 +0700

    update code c pro: pointer, arr, file, dslk, ds mang by le huu

---
+++ Temp_video/c2dslk_doituong.cpp
@@ -0,0 +1,15 @@
+/*
+power by le minh huu 22/12/2022 @LeHuu02 @copyright
+yeu cau:    - nhap xuat dslk doi tuong sinh vien: masv, ten, diem
+            - in thong tin sv tai Ha Noi
+            - them / xoa sinh vien thu k
+
+        NOTE: 
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    return 0;
+}
---
+++ Temp_video/c2dslk_songuyen.cpp
@@ -0,0 +1,15 @@
+/*
+power by le minh huu 22/12/2022 @LeHuu02 @copyright
+yeu cau:    - tao 1 danh sach lien ket so nguyen
+            - them phan tu vao / xoa phan tu di. vi tri x
+            - tinh tong phan tu trong ds
+
+        NOTE: 
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    return 0;
+}
